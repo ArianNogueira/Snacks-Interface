081c38cfc069d4989ae8e699054bdd35bc74f377
Arian <arianviegas12@gmail.com>
2026-08-24 23:36:38 -0300

    debug variaveis supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,13 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE URL CONFIGURADA:", !!supabaseUrl);
+console.log("SUPABASE KEY CONFIGURADA:", !!supabaseAnonKey);
+
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Configure NEXT_PUBLIC_SUPABASE_URL e NEXT_PUBLIC_SUPABASE_ANON_KEY no arquivo .env");
+  throw new Error(
+    "Configure NEXT_PUBLIC_SUPABASE_URL e NEXT_PUBLIC_SUPABASE_ANON_KEY no arquivo .env"
+  );
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
